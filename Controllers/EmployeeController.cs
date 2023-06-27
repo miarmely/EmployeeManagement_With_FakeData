ef4c1ca1c291a041d9d5778c8f13c9e2a50c64e8
Mert Akdemir <comp.eng.m.akdemir@gmail.com>
2023-06-27 13:31:38 +0300

    HttpGet ve HttpGet[id] Methodları Eklendi.

--- Controllers/EmployeeController.cs
+++ Controllers/EmployeeController.cs
@@ -12,5 +12,38 @@ namespace EmployeeManagement.Controllers
 
 
         public EmployeeController(ILogger<EmployeeController> logger) => _logger = logger;
+
+
+        [HttpGet]
+        public IActionResult DisplayEmployees()  // get all employees
+        {
+            // when database is empty
+            if (FakeData.Employees.Count == 0)
+                return NotFound(new
+                {
+                    statusCode = 404,
+                    message = "Database Is Empty!.."
+                });
+
+            _logger.LogInformation("All Employees Displayed.");
+            return Ok(FakeData.Employees);
+        }
+
+
+        [HttpGet("{id:int}")]
+        public IActionResult getEmployee([FromRoute(Name = "id")] int id)
+        {
+            // when not matched any employee
+            var entity = FakeData.Employees.Find(e => e.ID == id);
+            if (entity is null)
+                return BadRequest(new
+                {
+                    statusCode = 400,
+                    message = $"ID:{id} Is Not Exists On Database!.."
+                });
+
+            _logger.LogInformation($"ID:{id} Employee Displayed.");
+            return Ok(entity);
+        }
     }
 }
